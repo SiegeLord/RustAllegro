1bd6cd099d414e93e6eb293762034b1709e8e753
SiegeLord <slabode@aim.com>
2013-04-11 20:25:18 -0400

    Add more metadata

--- allegro.rs
+++ allegro.rs
@@ -1,4 +1,10 @@
-#[link(name = "allegro", vers = "5.1.6", author = "SiegeLord")];
+#[link(name = "allegro",
+       vers = "5.1.6",
+       author = "SiegeLord",
+       url = "https://github.com/SiegeLord/RustAllegro")];
+
+#[comment = "Allegro 5 core library Rust bindings"];
+#[license = "zlib"];
 #[crate_type = "lib"];
 
 pub use allegro5::altime::*;
