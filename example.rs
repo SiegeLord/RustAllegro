0a04b696ae71ae4e3e812500fd8ed78406fa0218
SiegeLord <slabode@aim.com>
2013-04-13 23:16:08 -0400

    Bound bitmap.h

--- example.rs
+++ example.rs
@@ -15,6 +15,10 @@ fn main()
 
 	io::println(fmt!("Display dimensions: %?", (al_get_display_width(d), al_get_display_height(d))));
 
+	let bmp = ~al_create_bitmap(300, 200).expect("creating bitmap");
+
+	io::println(fmt!("Bitmap dimensions: %?", (al_get_bitmap_width(bmp), al_get_bitmap_height(bmp))));
+
 	al_rest(2.0);
 	io::println("Done!");
 }
