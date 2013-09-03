da67166993b03976d3715c690fb1f8d49c6f7429
SiegeLord <slabode@aim.com>
2013-09-03 19:21:37 -0400

    Bind some more of altime.h

--- src/ffi/altime.rs
+++ src/ffi/altime.rs
@@ -1,4 +1,11 @@
 use std::libc::*;
 
+pub struct ALLEGRO_TIMEOUT
+{
+	__pad1__: uint64_t,
+	__pad2__: uint64_t,
+}
+
 externfn!(fn al_get_time() -> c_double)
 externfn!(fn al_rest(seconds: c_double))
+externfn!(fn al_init_timeout(timeout: *mut ALLEGRO_TIMEOUT, seconds: c_double))
