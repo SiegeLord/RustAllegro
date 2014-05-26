acd63feeaa969d8cf5c03eab1c2fad1c6deec7dd
Travis <travis@travis-ci.org>
2014-05-26 11:01:00 +0000

    Update docs from Travis build 88

---
+++ doc/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['allegro5'] = {EventQueue:"allegro5/struct.EventQueue.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
