eaac48fb7d0aee961adbe0cd102206e3a7fd30ee
Travis <travis@travis-ci.org>
2014-05-31 15:32:16 +0000

    Update docs from Travis build 91

---
+++ doc/implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['allegro5'] = {Color:"allegro5/struct.Color.html",Transform:"allegro5/transformations/struct.Transform.html",Color:"allegro5/struct.Color.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
---
+++ doc/implementors/core/ops/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['allegro5'] = {Transform:"allegro5/transformations/struct.Transform.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
