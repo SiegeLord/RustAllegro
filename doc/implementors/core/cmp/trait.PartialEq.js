626e96ed8e8f2c99ffdf67b6eceaa4a6f980c9a0
Travis <travis@travis-ci.org>
2014-07-02 21:42:51 +0000

    Update docs from Travis build 98

---
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['allegro5'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='allegro5/key/type.KeyCode.html' title='allegro5::key::KeyCode'>KeyCode</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='allegro5/key/type.KeyCode.html' title='allegro5::key::KeyCode'>KeyCode</a>",];
+implementors['allegro_audio'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='allegro_audio/properties/type.AudioDepth.html' title='allegro_audio::properties::AudioDepth'>AudioDepth</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/ops/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['allegro5'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a>&lt;<a class='struct' href='allegro5/ffi/transformations/struct.ALLEGRO_TRANSFORM.html' title='allegro5::ffi::transformations::ALLEGRO_TRANSFORM'>ALLEGRO_TRANSFORM</a>&gt; for <a class='struct' href='allegro5/transformations/struct.Transform.html' title='allegro5::transformations::Transform'>Transform</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
