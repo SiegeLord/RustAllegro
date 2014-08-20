dc0a0916a338a836cda2671709448fd141b2f620
Travis <travis@travis-ci.org>
2014-08-20 13:56:06 +0000

    Update docs from Travis build 114

---
+++ doc/implementors/core/ops/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['allegro'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a>&lt;<a class='struct' href='allegro/ffi/transformations/struct.ALLEGRO_TRANSFORM.html' title='allegro::ffi::transformations::ALLEGRO_TRANSFORM'>ALLEGRO_TRANSFORM</a>&gt; for <a class='struct' href='allegro/transformations/struct.Transform.html' title='allegro::transformations::Transform'>Transform</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
