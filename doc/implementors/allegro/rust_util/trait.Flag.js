c7041d40e4d2818cd557473ac521abbe8eb7d9c5
Travis <travis@travis-ci.org>
2014-08-30 13:22:28 +0000

    Update docs from Travis build 117

---
+++ doc/implementors/allegro/rust_util/trait.Flag.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors['allegro'] = [];
+implementors['allegro_ttf'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='allegro/rust_util/trait.Flag.html' title='allegro::rust_util::Flag'>Flag</a> for <a class='struct' href='allegro_ttf/struct.TtfFlags.html' title='allegro_ttf::TtfFlags'>TtfFlags</a>",];
+implementors['allegro_dialog'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='allegro/rust_util/trait.Flag.html' title='allegro::rust_util::Flag'>Flag</a> for <a class='struct' href='allegro_dialog/struct.MessageBoxFlags.html' title='allegro_dialog::MessageBoxFlags'>MessageBoxFlags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
