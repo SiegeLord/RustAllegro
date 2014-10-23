33fed13ca37313258aa2c5e9f161ef7528b19254
Travis <travis@travis-ci.org>
2014-10-23 11:37:27 +0000

    Update docs from Travis build 121

---
+++ doc/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['allegro'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;<a class='enum' href='allegro/enum.Event.html' title='allegro::Event'>Event</a>&gt; for <a class='struct' href='allegro/struct.EventQueue.html' title='allegro::EventQueue'>EventQueue</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
