0eec4a533b74608c1e204471db8a3c6f63ec9d28
Travis <travis@travis-ci.org>
2014-12-17 16:40:33 +0000

    Update docs from Travis build 129

--- doc/implementors/core/cmp/trait.PartialEq.js
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
-implementors['allegro_audio'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='allegro_audio/properties/enum.AudioDepth.html' title='allegro_audio::properties::AudioDepth'>AudioDepth</a>",];
 implementors['allegro'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='allegro/enum.KeyCode.html' title='allegro::KeyCode'>KeyCode</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='allegro/enum.KeyCode.html' title='allegro::KeyCode'>KeyCode</a>",];
+implementors['allegro_audio'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='allegro_audio/properties/enum.AudioDepth.html' title='allegro_audio::properties::AudioDepth'>AudioDepth</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
