1084cd9ff027894e3dc2177fde61d71c665d1163
Travis <travis@travis-ci.org>
2015-01-08 06:02:00 +0000

    Update docs from Travis build 142

--- doc/implementors/core/iter/trait.Iterator.js
+++ doc/implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['allegro'] = ["<a class='stability Unstable' title='Unstable: just split up for object safety'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='allegro/struct.EventQueue.html' title='allegro::EventQueue'>EventQueue</a>",];
+implementors['allegro'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='allegro/struct.EventQueue.html' title='allegro::EventQueue'>EventQueue</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
