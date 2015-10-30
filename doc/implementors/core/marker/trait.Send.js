e42deaa004de57c6cc88a6d6a0c2a7ce3ef439a2
Travis <travis@travis-ci.org>
2015-10-30 06:05:03 +0000

    Update docs from Travis build 236

--- doc/implementors/core/marker/trait.Send.js
+++ doc/implementors/core/marker/trait.Send.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['allegro'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='allegro/struct.MemoryBitmap.html' title='allegro::MemoryBitmap'>MemoryBitmap</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='allegro/struct.Transform.html' title='allegro::Transform'>Transform</a>",];
+implementors['allegro'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='allegro/struct.MemoryBitmap.html' title='allegro::MemoryBitmap'>MemoryBitmap</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='allegro/struct.Transform.html' title='allegro::Transform'>Transform</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
