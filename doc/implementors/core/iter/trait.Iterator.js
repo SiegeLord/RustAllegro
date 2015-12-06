5bd1f3099f0006e04a218f6ee0b99f19cdbe07dc
Travis <travis@travis-ci.org>
2015-12-06 02:38:25 +0000

    Update docs from Travis build 273

--- doc/implementors/core/iter/trait.Iterator.js
+++ doc/implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['allegro'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='allegro/struct.EventQueue.html' title='allegro::EventQueue'>EventQueue</a>",];
+implementors['allegro'] = ["impl&lt;'l&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='allegro/struct.ConfigSection.html' title='allegro::ConfigSection'>ConfigSection</a>&lt;'l&gt;","impl&lt;'l&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='allegro/struct.ConfigEntry.html' title='allegro::ConfigEntry'>ConfigEntry</a>&lt;'l&gt;","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='allegro/struct.EventQueue.html' title='allegro::EventQueue'>EventQueue</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
