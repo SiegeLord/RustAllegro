d90d897c3b7b8471e3d0e4e484b576101af2deb8
Travis <travis@travis-ci.org>
2017-04-28 05:06:51 +0000

    Update docs from Travis build 794

--- doc/implementors/core/cmp/trait.PartialEq.js
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
-implementors["allegro"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='allegro/keycodes/enum.KeyCode.html' title='allegro::keycodes::KeyCode'>KeyCode</a>",];
-implementors["allegro_audio"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='allegro_audio/enum.AudioDepth.html' title='allegro_audio::AudioDepth'>AudioDepth</a>",];
+implementors["allegro"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"allegro/keycodes/enum.KeyCode.html\" title=\"enum allegro::keycodes::KeyCode\">KeyCode</a>",];
+implementors["allegro_audio"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"allegro_audio/enum.AudioDepth.html\" title=\"enum allegro_audio::AudioDepth\">AudioDepth</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
