a72700a91a31e89782d2a7cbdd4370d7ad25de00
Travis <travis@travis-ci.org>
2017-08-06 06:28:10 +0000

    Update docs from Travis build 895

--- doc/allegro/core/sidebar-items.js
+++ doc/allegro/core/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["FLIP_HORIZONTAL",""],["FLIP_NONE",""],["FLIP_VERTICAL",""]],"enum":[["BlendMode",""],["BlendOperation",""]],"struct":[["BitmapDrawingFlags",""],["Core",""]]});
+initSidebarItems({"constant":[["FLIP_HORIZONTAL",""],["FLIP_NONE",""],["FLIP_VERTICAL",""]],"enum":[["BlendMode",""],["BlendOperation",""]],"struct":[["BitmapDrawingFlags",""],["Core","Type through which you'll be doing most your interaction with Allegro."],["MonitorInfo","Extents of a monitor."]]});
