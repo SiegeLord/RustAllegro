9840af5016920e19e7ed4428bd6a3c0f65c5627b
Travis <travis@travis-ci.org>
2017-09-01 06:07:00 +0000

    Update docs from Travis build 921

---
+++ doc/implementors/core/ops/bit/trait.BitOr.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["allegro"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"allegro/bitmap_like/struct.BitmapFlags.html\" title=\"struct allegro::bitmap_like::BitmapFlags\">BitmapFlags</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"allegro/core/struct.BitmapDrawingFlags.html\" title=\"struct allegro::core::BitmapDrawingFlags\">BitmapDrawingFlags</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"allegro/display/struct.DisplayFlags.html\" title=\"struct allegro::display::DisplayFlags\">DisplayFlags</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"allegro/joystick/struct.StickFlags.html\" title=\"struct allegro::joystick::StickFlags\">StickFlags</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"allegro/keycodes/struct.KeyModifier.html\" title=\"struct allegro::keycodes::KeyModifier\">KeyModifier</a>",];
+implementors["allegro_dialog"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"allegro_dialog/struct.MessageBoxFlags.html\" title=\"struct allegro_dialog::MessageBoxFlags\">MessageBoxFlags</a>",];
+implementors["allegro_ttf"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOr.html\" title=\"trait core::ops::bit::BitOr\">BitOr</a> for <a class=\"struct\" href=\"allegro_ttf/struct.TtfFlags.html\" title=\"struct allegro_ttf::TtfFlags\">TtfFlags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
