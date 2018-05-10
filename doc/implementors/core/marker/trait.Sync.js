acb85bf7b4eada320a53b3b012d1490d7591a3e6
Travis <travis@travis-ci.org>
2018-05-10 05:03:15 +0000

    Update docs from Travis build 1175

--- doc/implementors/core/marker/trait.Sync.js
+++ doc/implementors/core/marker/trait.Sync.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
-implementors["allegro"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"allegro/config/struct.Config.html\" title=\"struct allegro::config::Config\">Config</a>",];
+implementors["allegro"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"allegro/bitmap/struct.Bitmap.html\" title=\"struct allegro::bitmap::Bitmap\">Bitmap</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"allegro/bitmap/struct.SubBitmap.html\" title=\"struct allegro::bitmap::SubBitmap\">SubBitmap</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"allegro/config/struct.Config.html\" title=\"struct allegro::config::Config\">Config</a>",];
+implementors["allegro_font"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"allegro_font/struct.Font.html\" title=\"struct allegro_font::Font\">Font</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
