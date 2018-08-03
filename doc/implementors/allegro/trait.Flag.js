730f3d0e63062510756f944584cec38a5eb9dd83
Travis <travis@travis-ci.org>
2018-08-03 03:04:51 +0000

    Update docs from Travis build 1263

---
+++ doc/implementors/allegro/trait.Flag.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["allegro"] = [{text:"impl <a class=\"trait\" href=\"allegro/trait.Flag.html\" title=\"trait allegro::Flag\">Flag</a> for <a class=\"struct\" href=\"allegro/bitmap_like/struct.BitmapFlags.html\" title=\"struct allegro::bitmap_like::BitmapFlags\">BitmapFlags</a>",synthetic:false,types:["allegro::bitmap_like::BitmapFlags"]},{text:"impl <a class=\"trait\" href=\"allegro/trait.Flag.html\" title=\"trait allegro::Flag\">Flag</a> for <a class=\"struct\" href=\"allegro/core/struct.BitmapDrawingFlags.html\" title=\"struct allegro::core::BitmapDrawingFlags\">BitmapDrawingFlags</a>",synthetic:false,types:["allegro::core::BitmapDrawingFlags"]},{text:"impl <a class=\"trait\" href=\"allegro/trait.Flag.html\" title=\"trait allegro::Flag\">Flag</a> for <a class=\"struct\" href=\"allegro/display/struct.DisplayFlags.html\" title=\"struct allegro::display::DisplayFlags\">DisplayFlags</a>",synthetic:false,types:["allegro::display::DisplayFlags"]},{text:"impl <a class=\"trait\" href=\"allegro/trait.Flag.html\" title=\"trait allegro::Flag\">Flag</a> for <a class=\"struct\" href=\"allegro/joystick/struct.StickFlags.html\" title=\"struct allegro::joystick::StickFlags\">StickFlags</a>",synthetic:false,types:["allegro::joystick::StickFlags"]},{text:"impl <a class=\"trait\" href=\"allegro/trait.Flag.html\" title=\"trait allegro::Flag\">Flag</a> for <a class=\"struct\" href=\"allegro/keycodes/struct.KeyModifier.html\" title=\"struct allegro::keycodes::KeyModifier\">KeyModifier</a>",synthetic:false,types:["allegro::keycodes::KeyModifier"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
