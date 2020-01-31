10d97e961ce617292fe8bd4cd90b210b158b5289
Travis <travis@travis-ci.org>
2020-01-31 05:06:38 +0000

    Update docs from Travis build 1827

--- doc/implementors/core/cmp/trait.Eq.js
+++ doc/implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["allegro_primitives"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"allegro_primitives/enum.VertexAttrStorage.html\" title=\"enum allegro_primitives::VertexAttrStorage\">VertexAttrStorage</a>",synthetic:false,types:["allegro_primitives::VertexAttrStorage"]},];
+implementors["allegro_primitives"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"allegro_primitives/enum.VertexAttrStorage.html\" title=\"enum allegro_primitives::VertexAttrStorage\">VertexAttrStorage</a>","synthetic":false,"types":["allegro_primitives::VertexAttrStorage"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/marker/trait.StructuralEq.js
+++ doc/implementors/core/marker/trait.StructuralEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["allegro_primitives"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"allegro_primitives/enum.VertexAttrStorage.html\" title=\"enum allegro_primitives::VertexAttrStorage\">VertexAttrStorage</a>",synthetic:false,types:["allegro_primitives::VertexAttrStorage"]},];
+implementors["allegro_primitives"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"allegro_primitives/enum.VertexAttrStorage.html\" title=\"enum allegro_primitives::VertexAttrStorage\">VertexAttrStorage</a>","synthetic":false,"types":["allegro_primitives::VertexAttrStorage"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
